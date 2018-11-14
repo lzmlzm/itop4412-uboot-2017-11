355e2466926d62e14838450e66dd67ca6e00cd5b
lzmlzm <1198492751@qq.com>
2018-11-14 21:21:05 +0800

    update

---
+++ u-boot-2017.11/build_uboot.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+echo "Start compile Uboot-2017.11...."
+
+make distclean
+
+make itop4412_config
+
+if ! make -j4; then
+	exit 1;
+fi
+
+echo "making BL2....."
+split -b 14336 u-boot.bin bl2
+#split -b 14336 spl/skycui4412-spl.bin bl2
+./chksum
+	
+echo "cat bl1+bl2 to u-boot-itop4412.bin"
+cat E4412.S.BL1.SSCR.EVT1.1.bin checksum_bl2_14k.bin all00_padding.bin u-boot.bin > u-boot-itop4412.bin
+	
+rm bl2a*
+
+echo "Compile finish!!"
+
